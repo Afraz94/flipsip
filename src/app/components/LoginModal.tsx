a35a3c2c42b1b6b814d7e5790a71ee27378b5f00
Afraz Ali <afraz.ali.here@gmail.com>
2025-06-06 03:51:30 +0530

    Added 'use cliemnt' directive

--- src/app/components/LoginModal.tsx
+++ src/app/components/LoginModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import type { User } from "./Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- src/app/components/OrderModal.tsx
+++ src/app/components/OrderModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import type { User } from "./Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
